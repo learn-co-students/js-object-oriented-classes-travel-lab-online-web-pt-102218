7865801ac81174c30f8cf4a42f8d1e58ae29c1b5
Matthew Sherman <msherman0801@gmail.com>
2019-05-24 11:05:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+class Driver {
+    constructor(name, startDate) {
+        this.name = name;
+        this.startDate = new Date(startDate);
+    }
 
+    yearsExperienceFromBeginningOf(year) {
+        console.log(this.startDate.toString().split(' ')[3])
+        return year - this.startDate.toString().split(' ')[3]
+    }
+
+}
+
+let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+];
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation
+        this.endingLocation = endingLocation
+      }
+      blocksTravelled() {
+        let vertical = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+        let horizontal = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+        return (vertical + horizontal)
+      }
+      estimatedTime(peakHours) {
+         if (peakHours === true) {
+           return (this.blocksTravelled()/2)
+         } else {return (this.blocksTravelled()/3)
+         }
+      }
+}
